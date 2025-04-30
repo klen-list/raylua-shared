fcbe8f87ddd2668aad42359c9a785c6bb690b128
Klen_list <a.gorbunov@dro.red>
2025-04-30 05:51:46 +0300

    🔥📝 Remove useless code from log lib + add doc for `log` func

--- log.lua
+++ log.lua
@@ -1,21 +1,15 @@
-local ffi = require "ffi"
-
-if WINDOWS or LINUX then
-	os.execute "mkdir logs"
-	
-	ffi.cdef "void* freopen(const char*, const char*, void*)"
-	
-	local filepath = "./logs/" .. os.date('log-%Y-%m-%d_%H-%M-%S.txt')
-	
-	ffi.C.freopen(filepath, "w", io.stdout)
-	ffi.C.freopen(filepath, "a", io.stderr)
-end
+---@author Klen_list <https://github.com/klen-list>
 
 LOG_DEBUG = rl.LOG_DEBUG
 LOG_INFO = rl.LOG_INFO
 LOG_WARNING = rl.LOG_WARNING
 LOG_ERROR = rl.LOG_ERROR
 
-function log(level, text, ...)
-	rl.TraceLog(level, text, ...);  
+---Основная функция логгирования.
+---@param level integer
+---@param text string
+---@param ... string
+---@return nil
+function Log(level, text, ...)
+	rl.TraceLog(level, text, ...)
 end
